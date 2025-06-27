427921513bfd608f620a8f523ffb041e10ec1ff2
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2025-06-27 08:28:20 +0530

    upload imege fix

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imagini"
+      uploadPreset="imagina"
       options={{
         multiple: false,
         resourceType: "image",
